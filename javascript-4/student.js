afdf0f9eb6f338dbf42adbfdcdae129a19f4cf51
dovahlana <atennison25@gmail.com>
2020-01-16 16:33:04 -0600

    commit

--- javascript-4/student.js
+++ javascript-4/student.js
@@ -4,10 +4,11 @@
 //////////////////Step 1////////////////////
 // Create a new array called 'faveColors' and assign it a value of your three favorite colors as strings.
 
-// CODE HERE
+let faveColors = ['black','purple','blue'];
 
-//////////////////Step 2////////////////////
-// Create an object called 'me' that has these keys: firstname, superHeroName, homeTown, superPowers, superPowerXP, and profileImage. 
+// //////////////////Step 2////////////////////
+// // Create an object called 'me' that has these keys: firstname, superHeroName, homeTown, superPowers, 
+// superPowerXP, and profileImage. 
 
 // The firstName key should have a value of your name as a string. 
 
@@ -21,27 +22,71 @@
 
 // The profileImage key value should be a method that returns this url `https://randomuser.me/api/portraits/med/lego/${Math.floor(Math.random() * 10) + 1 }.jpg`.
 
-// CODE HERE
+let me = {
+    firstName: 'Alana',
+    superHeroName: 'Dovahkiin',
+    homeTown: 'Anderson',
+    superPowers: ['Fus','Ro','Dah'],
+    superPowerXP: Math.floor(Math.random() * 100) + 1 ,
+    profileImage: `https://randomuser.me/api/portraits/med/lego/${Math.floor(Math.random() * 10) + 1 }.jpg`
+}
 
 //////////////////Step 3////////////////////
-// In this step, you will create three variables that will hold some data from your me object. The first variable you create should be 'regularName', and it should receive its value from the value of your firstName key on the me object above. The next variable is 'superName' which should have its value assigned as the value of the superHeroName property on the me object. The last variable you should create is 'homeTown' which will be the value of homeTown on the me object
+// // In this step, you will create three variables that will hold some data from your me object. The first variable you create should be 'regularName', 
+// and it should receive its value from the value of your firstName key on the me object above. The next variable is 
+// 'superName' which should have its value assigned as the value of the superHeroName property on the me object. 
+// The last variable you should create is 'homeTown' which will be the value of homeTown on the me object
+let regularName = me.firstName;
+let superName = me.superHeroName;
+let homeTown = me.homeTown;
 
-// CODE HERE
 
 //////////////////Step 4////////////////////
-//Create a function called 'setColor' that takes in one parameter called 'arr' that is an array of colors. Make sure that we only ever have three colors by using splice to trim the array to just 3 colors. 
+// //Create a function called 'setColor' that takes in one parameter called 'arr' that is an array of colors. 
+// Make sure that we only ever have three colors by using splice to trim the array to just 3 colors. 
 
-// Next, create a for loop to loop over the remaining three colors. If any of the colors is 'blue', change it's value to '#4D4DFF' (which is just a more appealing shade of blue). Outside of the for loop but still inside of setColor, invoke the function called 'background' which will take in three arguments. These arguments should be the three items remaining in your colors array. 
+// Next, create a for loop to loop over the remaining three colors. If any of the colors is 'blue',
+//  change it's value to '#4D4DFF' (which is just a more appealing shade of blue). Outside of the for loop but still
+//   inside of setColor, invoke the function called 'background' which will take in three arguments. These arguments 
+//   should be the three items remaining in your colors array. 
 
-// CODE HERE
+function setColor(arr) {
+    setColor.splice(0,3);
 
-//////////////////Step 5////////////////////
-//Create a function called 'setPowers' that takes in 'arr' as a parameter. In your setPowers function, loop over the arr parameter and run a function we created called createLi(), which will take each item of the array as an argument. Remember, you did not create the createLi function. The createLi function is a function we created that determines how the content is displayed.
+for (i = 0; i < setColor.length; i++) {
+    if (i === 'blue') {
+    return '#4D4DFF'; 
+}
+}
+background(item1,item2,item3)
+}
 
-// CODE HERE
+
+// //////////////////Step 5////////////////////
+// //Create a function called 'setPowers' that takes in 'arr' as a parameter. In your setPowers function, loop over 
+// the arr parameter and run a function we created called createLi(), which will take each item of the array as an 
+// argument. Remember, you did not create the createLi function. The createLi function is a function we created that 
+// determines how the content is displayed.
+
+function setPowers(arr) {
+    for(i = 0; i < setColor.length; i++) {
+        createLi();
+    }
+}
 
 
-//////////////////Step 6////////////////////
-//Lastly, create a function called 'redactInfo' that will take in an object called 'obj' as a parameter. Now, imagine your super hero needs to go undercover, and you need to remove all info about them. That is what this function will do. In your function, loop over the obj parameter passed in, and change each keys value to 'redacted'. Outside of the loop but still in the redactInfo function, run the function redacted() which is a function we created that will update the text on the screen.
 
-// CODE HERE
+// //////////////////Step 6////////////////////
+// //Lastly, create a function called 'redactInfo' that will take in an object called 'obj' as a parameter. 
+// Now, imagine your super hero needs to go undercover, and you need to remove all info about them. That is what this 
+// function will do. In your function, loop over the obj parameter passed in, and change each keys value to 'redacted'. 
+// Outside of the loop but still in the redactInfo function, run the function redacted() which is a function we created 
+// that will update the text on the screen.
+
+function redactInfo(obj) {
+    for(let key in obj ) {
+        return 'redacted';
+    }
+    redacted();
+}
+}
